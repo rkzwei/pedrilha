4f9d7c31fec1cfb5277c5e2d22c683e76d36768e
rkzwei <rk@rkzwei.dev>
2026-06-24 02:13:00 -0300

    chore: gitignore *.bat and *_out.txt scratch files; untrack cleanup artifacts

--- cleanup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-del final_clean.txt 2>nul
-"C:\Program Files\Git\bin\git.exe" add -A
-"C:\Program Files\Git\bin\git.exe" commit -m "chore: remove temp files (commit_out, do_commit, final_clean)"
-echo done
