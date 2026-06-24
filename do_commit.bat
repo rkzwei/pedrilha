060ac20e8e0ea2221be79a356435d218046eece0
rkzwei <rk@rkzwei.dev>
2026-06-24 02:12:41 -0300

    chore: remove temp files (commit_out, do_commit, final_clean)

--- do_commit.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\Program Files\Git\bin\git.exe" rm --cached commit_msg.txt commit_out.txt do_commit.bat gs.txt gc.txt ga.txt 2>nul
-del commit_msg.txt commit_out.txt commit_out.txt gs.txt gc.txt ga.txt 2>nul
-"C:\Program Files\Git\bin\git.exe" add -A
-"C:\Program Files\Git\bin\git.exe" commit -m "chore: remove temp commit artifacts"
