b7f1944273654817560a50722291bb52bffbd95f
rkzwei <rk@rkzwei.dev>
2026-06-24 02:10:58 -0300

    chore: remove temp commit artifacts

--- do_commit.bat
+++ do_commit.bat
@@ -1,3 +1,5 @@
 @echo off
+"C:\Program Files\Git\bin\git.exe" rm --cached commit_msg.txt commit_out.txt do_commit.bat gs.txt gc.txt ga.txt 2>nul
+del commit_msg.txt commit_out.txt commit_out.txt gs.txt gc.txt ga.txt 2>nul
 "C:\Program Files\Git\bin\git.exe" add -A
-"C:\Program Files\Git\bin\git.exe" commit -F commit_msg.txt
+"C:\Program Files\Git\bin\git.exe" commit -m "chore: remove temp commit artifacts"
